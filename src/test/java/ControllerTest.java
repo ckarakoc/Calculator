2eb423145f7b1ade383c7c7fef792eeb82be05e2
Celal <c.karakoc@student.tudelft.nl>
2016-01-29 16:42:26 +0100

    committing all day, every day

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by Celal on 29-1-2016.
+ */
+public class ControllerTest {
+
+    public class MyClassTest {
+        @Test
+        public void testSomething() {
+
+        }
+    }
+}
